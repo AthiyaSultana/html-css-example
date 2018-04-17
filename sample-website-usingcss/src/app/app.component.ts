12694b1d517819d9ec712bebcf5eba963ed5a09d
Athiya Sultana <athiya@olivetech.net>
2018-04-17 19:01:45 +0530

    Added design for home login and register

--- sample-website-usingcss/src/app/app.component.ts
+++ sample-website-usingcss/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {LoginComponent} from './login/login.component';
+import {MatDialog} from '@angular/material';
+import {RegisterComponent} from './register/register.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private Dialog: MatDialog) {
+  }
+
+  openDialog() {
+    const dialog = this.Dialog.open(LoginComponent, {
+      height: '400px',
+      width: '400px',
+    });
+  }
+
+  openRegisterDialog() {
+    const dialog = this.Dialog.open(RegisterComponent, {
+      height: '500px',
+      width: '400px',
+    });
+  }
 }
--- sample-website-usingcss/src/app/app.module.ts
+++ sample-website-usingcss/src/app/app.module.ts
@@ -1,17 +1,21 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {AppComponent} from './app.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {LoginComponent} from './login/login.component';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import {MatDialogModule, MatFormFieldModule, MatInputModule, MatSelectModule} from '@angular/material';
+import {MatButtonModule} from '@angular/material/button';
+import {RegisterComponent} from './register/register.component';
+import {ServicesComponent} from './services/services.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: 'login', component: LoginComponent}
+  {path: 'services', component: ServicesComponent}
 ];
 
 @NgModule({
@@ -19,14 +23,23 @@ const routes: Routes = [
     AppComponent,
     DashboardComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    RegisterComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
+    MatDialogModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatInputModule,
     RouterModule.forRoot(routes, {enableTracing: true})
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [RegisterComponent, LoginComponent]
 })
 export class AppModule {
 }
